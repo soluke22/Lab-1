86f646e4cbd6d28813129fda10a10e7f0715427b
soluke22 <soluke22@gmail.com>
2019-10-28 20:38:19 -0400

    added lines that allocate space for the string to be inputted

--- lab1.s
+++ lab1.s
@@ -16,3 +16,9 @@ main:
 	la $a0, Ask_Input #loads Ask_Input and stores it into register a0
 	li $v0, 4 #loads the value 4 into the register v0, 4 has the op code for print string 
 	syscall #reads the register v0 for its op code, see it's 4 and prints the string located in register a0
+
+	la $a0, insert_into #sets a0 to a space allocated for writing words
+	la $a1, insert_into #gets the length of a1 so we can't go over the memory limit
+	li $v0, 8 #loads the value 8 into the register v0, 8 has the op code for getting a string from the user into register v0
+	syscall #reads the register v0 for op code, sees 8 and asks the user to input a string
+
