fcd61ddb668977da27a1293994ea7dfaebacae98
soluke22 <soluke22@gmail.com>
2019-10-28 20:40:59 -0400

    added code to store the memory from tell_output

--- lab1.s
+++ lab1.s
@@ -22,3 +22,8 @@ main:
 	li $v0, 8 #loads the value 8 into the register v0, 8 has the op code for getting a string from the user into register v0
 	syscall #reads the register v0 for op code, sees 8 and asks the user to input a string
 
+	la $a0, Tell_Output #loads addres from the memory and stores it into a0
+	li $v0, 4 $loads the value 4 into the register v0, 4 is the op code for print string
+	syscall #reads the op code in v0, sees 4 and prints the string located in a0
+
+	
