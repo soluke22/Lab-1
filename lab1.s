a34d6d74c2e9feb62588bd31083d3fe449af13ae
soluke22 <soluke22@gmail.com>
2019-10-29 22:34:11 -0400

    deleted everything and restarted now that i have a grasp of things

--- lab1.s
+++ lab1.s
@@ -13,43 +13,19 @@
 str:
 	.space 32
 
-Ask_Input:
-	.asciiz "\Please enter a string of 10 characters to print \n"
 
-Tell_Output:
-	.asciiz "\You typed in: "
+output:
+	.asciiz "\The result is:  "
 
 .text
 
 main:
+	li $v0, 4
+	la $a0, str
+	li $a1, 11
+	syscall
 
-	la $a0, Ask_Input #loads Ask_Input and stores it into register a0
-	li $v0, 4 #loads the value 4 into the register v0, 4 has the op code for print string 
-	syscall #reads the register v0 for its op code, see it's 4 and prints the string located in register a0
 
-	la $a0, str #sets a0 to a space allocated for writing words
-	la $a1, str #gets the length of a1 so we can't go over the memory limit
-	li $v0, 8 #loads the value 8 into the register v0, 8 has the op code for getting a string from the user into register v0
-	syscall #reads the register v0 for op code, sees 8 and asks the user to input a string
-
-	looper:
-		add $t0, $zero, $zero #i = 0
-	L1: add $t1, $a0, $t0 #t1 now has the address of the  index of the string
-		lb $t2, 0($t1) #t2 now has the value of the  index of the string
-		addi, $t2, $t2, -48 #convert the character to ASCII
-		sw $a0, 0($t2) #store it into register a0
-		li $v0, 4 #print the string in register a0
-		syscall#call so it prints
-		addi $t0,$t0,1#increment
-		j L1
-
-	la $a0, Tell_Output #loads addres from the memory and stores it into a0
-	li $v0, 4 #loads the value 4 into the register v0, 4 is the op code for print string
-	syscall #reads the op code in v0, sees 4 and prints the string located in a0
-
-	la $a0, str #loads the insert_into address into a0
-	syscall #reads v0, sees 4 and prints the string in a0
-	li $v0, 4 #loads 4 into register v0 which is the op code for print string
 	
 	li $v0, 10 #loads the op code to exit the program
 	syscall #exits the program after reading v0=-0
